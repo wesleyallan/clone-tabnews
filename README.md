24cfcdb67d490a949fc2ea3780b3e10f1b482b4e
Wesley Silva <wesley.allansilva@gmail.com>
2023-09-16 16:51:30 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 📰 Clone do projeto TabNews
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,11 @@ function Home() {
   return (
     <>
       <h1>Ola!</h1>
-      <p>Como Thomas Hobbes dizia <cite>O homem é o lobo do homem!</cite> 🐺</p>
+      <p>
+        Como Thomas Hobbes dizia <cite>O homem é o lobo do homem!</cite> 🐺
+      </p>
     </>
-  )
+  );
 }
 
 export default Home;
