33f31bd3476bbbe83e1058d1b09d08ab976c5b0d
Wesley Silva <wesley.allansilva@gmail.com>
2023-12-06 22:14:46 -0300

    add `.database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function getState(request, response) {
+import database from "../../../../infra/database.js";
+
+async function getState(request, response) {
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log(result);
   response.status(200).json({ database: "online" });
 }
 
