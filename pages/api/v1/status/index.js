39553fe9300053a5c09668ff368c430abb394c75
Wesley Silva <wesley.allansilva@gmail.com>
2023-12-19 21:20:01 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,25 @@
 import database from "infra/database.js";
 
 async function getState(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result);
-  response.status(200).json({ database: "online" });
+  const updateAt = new Date().toISOString();
+  const dbVersion = (await database.query("SHOW server_version"))?.rows[0].server_version;
+  const maxConnections = (await database.query("SHOW max_connections;"))?.rows[0].max_connections;
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnections = (await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  }))?.rows[0].count;
+
+  response.status(200).json({ 
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        opened_connections: openedConnections,
+        max_connections: parseInt(maxConnections)
+      }
+    },
+   });
 }
 
 export default getState;
